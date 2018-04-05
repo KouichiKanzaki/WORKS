27bbe64a33883752d7cdce79e1ef2ebb2c078037
DESKTOP-8CJPC52\kouichi <lilkfsm4@gmail.com>
2018-04-06 07:18:12 +0900

    css

---
+++ MyLaravel/resources/views/index.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="shortcut icon" href="../src/portfolio/K_logo.png" type="image/x-icon">
+	<title>神﨑幸逸 ポートフォリオサイト</title>
+	<link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+	<link rel="stylesheet" href="../css/bootstrap.min.css" />
+	<link rel="stylesheet" href="../css/style.css" />
+</head>
+<body>
+<div class="container-fruid">
+	<header class="row fixed-top">
+		<div class="col-12" id="header-color">
+			<div class="col-sm-12 text-center siteTitle">
+				<h4>Kanzaki Kouichi<br>Portfolio site</h4>
+			</div>
+			<nav class="navbar navbar-expand-sm">
+				<button class="navbar-toggler" type="button"
+					data-toggle="collapse" data-target="#collapse-items">
+					<span><i class="fas fa-bars fa-1x headerIcon"></i></span></button>					
+				<div class="collapse navbar-collapse" id="collapse-items">
+					<ul class="navbar-nav container">
+						<li class="nav-item col-lg-2 text-center navList">
+							<a class="nav-link" href="#">Works</a>
+						</li>
+						<li class="nav-item col-md-2 text-center navList">
+							<a class="nav-link" href="#">Request</a>
+						</li>
+						<li class="nav-item col-md-2 text-center navList">
+							<a class="nav-link" href="#">Skill</a>
+						</li>
+						<li class="nav-item col-md-2 text-center navList">
+							<a class="nav-link" href="#">Contact</a>
+						</li>
+						<li class="nav-item col-md-2 text-center navList">
+							<a class="nav-link" href="#">Bulletin</a>
+						</li>
+						<li class="nav-item col-md-2 text-center navList">
+							<a class="nav-link" href="#">Other</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+	</header>
+	<main class="row">
+		<div class="col-12">
+
+		……中略。コンテンツを記述……
+
+		</div>
+	</main>
+</div>
+</body>
+<script src="../js/jquery.min.js"></script>
+<script src="../js/popper.min.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+<script src="../js/script.js"></script>
+<!-- </body> -->
+</html>
