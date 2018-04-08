275abea154b9facdccc5c41309f6e0b032fa39eb
DESKTOP-8CJPC52\kouichi <lilkfsm4@gmail.com>
2018-04-08 22:21:19 +0900

    javascript

--- MyLaravel/js/script.js
+++ MyLaravel/js/script.js
@@ -1,1 +1,14 @@
-// alert("KK");
+//alert();
+$(function(){
+
+   // ナビゲーションをクリック
+   $("a[href^='#']").click(function() {
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? "html" : href);
+      var position = target.offset().top - 100;
+
+      // スムーススクロール
+      $("body,html").animate({scrollTop:position}, 400, "swing");
+      return false;
+   });
+});
